7468543559895be1b7d13aeffa131f316670a1b7
sauhard22 <73341891+sauhard22@users.noreply.github.com>
2021-05-12 01:38:35 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -6,56 +6,11 @@ import React from '../assets/react.svg'
 function App() {
   return (
     <div>
-      <Container>
-        <Wrapper>
           <Header />
           <Main />
-        </Wrapper>
-      </Container>
     </div>
   );
 }
 
-const Container = styled.div`
-  background: #f9f9f9;
-  position: absolute;
-  top:0;
-  bottom:0;
-  left:0;
-  right:0;
-  img{
-    position:center;
-    display:flex;
-    justify-content:center;
-  }
-  
-  height: 100%;
-`
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(359deg);
-  }
-`
-
-
-const Wrapper = styled.div`
-  background-image: url(${React});
-  background-repeat:no-repeat;
-  background-position-x: 125%;
-  background-position-y: 5%;
-  background-attachment: fixed;
-  animation: ${rotation} 10s infinite linear;
-
-  
-  Header{
-    z-index:0;
-  }
-  Main{
-    z-index:0;
-  }
-`
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,70 +5,82 @@ import styled from 'styled-components'
 const Main = () => {
 
     const v1 = "{sauhard.is}"
+    const v2 = "{tools}"
+    const v3 = "{main.skills}"
 
-    return (   
-            <Container>
-                <Wrapper>
-                    <p className="head">{v1}</p>
-                    <br/>
-                    <p className="mid">Sauhard Srivastava</p>
-                    <br/>
-                    <p className="desc">Creative web developer, <br/> designer, editor, and a motion artist</p>
-                </Wrapper>
-                <Wrapper>
-                    <p className="head">{v1}</p>
-                    <br/>
-                    <p></p>
-                    <br/>
-                    <Description>
-                        <p className="desc dsp-itms">
-                            <li>JavaScript</li>
-                            <li>React.js</li>
-                            <li>Node.js</li>   
-                            <li>Adobe Photoshop</li>
-                            <li>Adobe Illustrator</li>
-                            <li>Adobe Premiere Pro</li>
-                            <li>Adobe After Effects</li>
-                        </p>
-                    </Description>
-                </Wrapper> 
-                <Wrapper className="bottom-value">
-                    <p className="head">{v1}</p>
-                    <br/>
-                    <p></p>
-                    <br/>
-                    <p className="desc">Creative web developer, <br/> designer, editor, and a motion artist</p>
-                </Wrapper>     
-            </Container>
+    return (
+        <Container>
+            <Wrapper>
+                <p className="head">{v1}</p>
+                <br />
+                <p className="mid">Sauhard Srivastava</p>
+                <br />
+                <p className="desc">Creative web developer, <br /> designer, editor, and a motion artist</p>
+            
+                <p className="head">{v2}</p>
+                <br />
+                <p></p>
+                <br />
+                <div>
+                <Tools>JavaScript<br/>HTML </Tools>
+                <Tools>React.js<br/> CSS</Tools>
+                <Tools>Node.js</Tools>
+                <Tools>Adobe Illustrator <br/>Figma</Tools>
+                <Tools>Adobe Photoshop<br/> Adobe After Effects</Tools>
+                <Tools>Adobe Premiere Pro<br/></Tools>
+                </div>
+                <p className="head">{v3}</p>
+                <br />
+                <p></p>
+                <br />
+                <div>
+                <Skills className="main-skills">UI/UX<br/>Web Development</Skills>
+                <Skills className="main-skills">Graphics Design, Motion Design, <br/>
+                Video Editing</Skills>
+                <Skills className="main-skills">Photograpgy, Filmmaking, <br/>
+                Storytelling</Skills>
+                </div>
+            </Wrapper>
+        </Container>
     )
 }
 
-const Description = styled.div`
-
-    .dsp-itms{
-        display:flex;
-    }
-
-    li{
-        list-style-type: none;
-        font-family: Berlin Sans FB;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 25px;
-        line-height: 27px;
-        margin-right: 50px;
-    }
-
+const Tools = styled.div`
+    float:left;
+    font-family: Berlin Sans FB;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+    line-height: 27px;
+    line-height: 50px;
+    padding-right: 50px;
+    display:flex;
 `
+
+const Skills = styled.div`
+    float:left;
+    font-family: Berlin Sans FB;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+    line-height: 27px;
+    line-height: 50px;
+    padding-right: 120px;
+`
+
 
 const Container = styled.div`
     
     padding: 90px 19%;
-    padding-bottom:0;
     
     p{
         margin:0;
 
+       
+
+    }
+ .mid-tools{
+        margin-top: 50px;
     }
 
     .head{
@@ -77,6 +89,8 @@ const Container = styled.div`
         font-size:  40px;
         font-weight:normal;
         line-height: 59px;
+        text-align: left;
+        margin-top:50px;
     }
     .mid{
         font-family: Montserrat;
@@ -97,10 +111,38 @@ const Container = styled.div`
 const Wrapper = styled.div`
     line-height: 30px;
     margin:0;
-    margin-bottom: 75px;
+    display:flex;
+    flex-direction: column;
+    .main-skills{
+        float: left;
+    }
 
     .bottom-value{
         margin-bottom: 0px;
+    }
+
+    .dsp-itms{
+        display: flex;
+        line-height: 50px;
+    }
+    div{
+        display: flex; 
+    }
+
+    p{
+        list-style-type: none;
+        font-family: Berlin Sans FB;
+        font-size: 25px;
+        
+    }
+
+    .li-padding{
+        padding-right:50px
+
+
+    }
+    span{
+        padding-right: 55px
     }
 
 `
